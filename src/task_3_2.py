b4679ff239009b354567040537538c2b4af5cce3
Maria Epikhova <epikhovamariya@gmail.com>
2022-06-06 21:20:09 +0300

    HW3: Mary Epikhova

---
+++ src/task_3_2.py
@@ -0,0 +1,7 @@
+num = int(input('How many guests came in the morning? '))
+if num>50:
+   print('Restaurant')
+elif num>=20 and num<=50:
+   print('Cafe')
+else:
+   print('Home')
---
+++ src/task_3_3.py
@@ -0,0 +1,5 @@
+str = input('Enter a string: ')
+if len(str)>10:
+    print(str + '!!!')
+elif len(str)<10:
+    print(str[1])
