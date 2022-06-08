afbf458ba08922e145c59fab174c2b88bc5b32d5
Maria Epikhova <epikhovamariya@gmail.com>
2022-06-08 17:17:01 +0300

    Add docstring & delete useless variable

--- src/task_4_1.py
+++ src/task_4_1.py
@@ -1,10 +1,11 @@
+"""Дан список целых чисел.
+Создать новый список, каждый элемент которого равен исходному элементу умноженному на -2"""
 from random import randint
 
 length = input('Enter a length for our list: ')
 while length.isdigit() is False:
     length = input('Incorrect input. Enter a length for our list: ')
 length = int(length)
-#arr = [randint(0, 10) for i in range(length)]
 arr = []
 for i in range(length):
     arr.append(randint(0, 10))
@@ -14,6 +15,5 @@ for i in arr:
     new_arr.append(i*(-2))
 print('New list. Solving with for-cycle:', new_arr)
 while len(new_arr) != length:
-    element = arr.pop()
-    new_arr.append(element*(-2))
+    new_arr.append(arr.pop() * (-2))
 print('New list. Solving with while-cycle:', new_arr)
